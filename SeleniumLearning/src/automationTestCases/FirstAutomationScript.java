9f242b7b0e03da0f4b7a18222a620df479fab671
ShaktiGaur <shakti.gaur93@gmail.com>
2019-05-19 19:17:20 +0530

    Removed Code for parent child concept

--- SeleniumLearning/src/automationTestCases/FirstAutomationScript.java
+++ SeleniumLearning/src/automationTestCases/FirstAutomationScript.java
@@ -22,7 +22,7 @@ public class FirstAutomationScript
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	    driver.get("https://www.seleniumeasy.com/test/table-search-filter-demo.html");
-		//driver.get("https://www.facebook.com/");
+		
 	   /* driver.findElement(By.id("user-message")).sendKeys("Hi");
 	    driver.findElement(By.xpath("//button[contains(text(),'Show Message')]")).click();
 	    driver.findElement(By.id("sum1")).sendKeys("1");
@@ -38,10 +38,6 @@ public class FirstAutomationScript
 		driver.findElement(By.cssSelector("button.btn btn-default")).click(); */
 		
 
-		
-		/*use of parent child concept
-		driver.findElement(By.xpath("//form[@id='login_form'] //input[@id='email']")).sendKeys("ABC");
-		driver.findElement(By.xpath("//form[@id='login_form'] //input[@id='pass']")).sendKeys("DEF"); */
 		
 		//use of identifying a cell in table
 	   List <WebElement> header = driver.findElements(By.xpath("//table[@id='task-table']/tbody/tr/th"));
